1be33fbc3bac53f0e75394df8aec1b309a895fff
Ryan Gjoraas <ryan.gjoraas@gmail.com>
2018-04-19 20:33:29 -0500

    updated code to fix presentingImagePicker

--- SimpleImagePicker/ViewController.swift
+++ SimpleImagePicker/ViewController.swift
@@ -19,12 +19,12 @@ UINavigationControllerDelegate {
     
     // MARK: ImagePicker function and alert for no camera
     // no camera on simulator:
-    func noCamera(){
-        let alertVC = UIAlertController(title: "No Camera", message: "Sorry, this device has no camera",preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "OK", style:.default, handler: nil)
-        alertVC.addAction(okAction)
-        present(alertVC, animated: true, completion: nil)
-    }
+//    func noCamera(){
+//        let alertVC = UIAlertController(title: "No Camera", message: "Sorry, this device has no camera",preferredStyle: .alert)
+//        let okAction = UIAlertAction(title: "OK", style:.default, handler: nil)
+//        alertVC.addAction(okAction)
+//        present(alertVC, animated: true, completion: nil)
+//    }
     
     // MARK: ImagePicker cancelling after picking image
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -36,6 +36,10 @@ UINavigationControllerDelegate {
         super.viewDidLoad()
         //to check that imageView is loading properly, give it a color:
         imagePickerView.backgroundColor = UIColor.lightGray
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+         takePhotoButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
     
     // MARK: ImagePicker Functions
@@ -58,17 +62,11 @@ UINavigationControllerDelegate {
     
     @IBAction func pickAnImageFromCamera(_ sender: UIBarButtonItem) {
         pickerController.delegate = self
+        pickerController.allowsEditing = false
+        pickerController.sourceType = UIImagePickerControllerSourceType.camera
+        pickerController.cameraCaptureMode = .photo
         present(pickerController, animated: true, completion: nil)
-        if UIImagePickerController.availableCaptureModes(for: .rear) != nil {
-            pickerController.allowsEditing = false
-            pickerController.sourceType = UIImagePickerControllerSourceType.camera
-            pickerController.cameraCaptureMode = .photo
-            present(pickerController, animated: true, completion: nil)
-            } else {
-                noCamera()
-            }
     }
-
 
     //MARK: Text Field Functions
     
