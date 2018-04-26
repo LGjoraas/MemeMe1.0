788023824d5886adb7c8cd2338342f9e0bf809f9
Ryan Gjoraas <ryan.gjoraas@gmail.com>
2018-04-25 21:20:36 -0500

    TextFields Done

---
+++ SimpleImagePicker/TextFieldsTopDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  textFieldsViewController.swift
+//  SimpleImagePicker
+//
+//  Created by Ryan Gjoraas on 4/24/18.
+//  Copyright © 2018 Developed by Gjoraas. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TextFieldsTopDelegate: NSObject, UITextFieldDelegate {
+    
+//     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        return true
+//    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text!.isEmpty {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true;
+    }
+    
+    
+    
+}
--- SimpleImagePicker/ViewController.swift
+++ SimpleImagePicker/ViewController.swift
@@ -9,38 +9,86 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate,
-UINavigationControllerDelegate {
+UINavigationControllerDelegate, UITextFieldDelegate {
  
+    // MARK: Properties
+    let pickerController = UIImagePickerController()
+    let textFieldsTopDelegate = TextFieldsTopDelegate()
+    let textFieldsBottomDelegate = TextFieldsBottomDelegate()
+    
+    // MARK: Attributes
+    let memeTextAttributes:[String: Any] = [
+        NSAttributedStringKey.strokeColor.rawValue: UIColor.black,
+        NSAttributedStringKey.foregroundColor.rawValue: UIColor.white,
+        NSAttributedStringKey.font.rawValue: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSAttributedStringKey.strokeWidth.rawValue: -3.0]
+    
+    
     // MARK: Outlets and Declarations
     @IBOutlet weak var imagePickerView: UIImageView!
     @IBOutlet weak var takePhotoButton: UIBarButtonItem!
-    let pickerController = UIImagePickerController()
-    
-    
-    // MARK: ImagePicker function and alert for no camera
-    // no camera on simulator:
-//    func noCamera(){
-//        let alertVC = UIAlertController(title: "No Camera", message: "Sorry, this device has no camera",preferredStyle: .alert)
-//        let okAction = UIAlertAction(title: "OK", style:.default, handler: nil)
-//        alertVC.addAction(okAction)
-//        present(alertVC, animated: true, completion: nil)
-//    }
-    
-    // MARK: ImagePicker cancelling after picking image
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        dismiss(animated: true, completion: nil)
-    }
+    @IBOutlet weak var topField: UITextField!
+    @IBOutlet weak var bottomField: UITextField!
     
     // MARK: ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
         //to check that imageView is loading properly, give it a color:
-        imagePickerView.backgroundColor = UIColor.lightGray
+        imagePickerView.backgroundColor = #colorLiteral(red: 0, green: 0.9914394021, blue: 1, alpha: 1)
+        imagePickerView.clipsToBounds = true
+        
+        self.topField.delegate = self.textFieldsTopDelegate
+        self.bottomField.delegate = self.textFieldsBottomDelegate
+        
+        topField.defaultTextAttributes = memeTextAttributes
+        bottomField.defaultTextAttributes = memeTextAttributes
+        
+        topField.text = "TOP"
+        topField.textAlignment = NSTextAlignment.center
+        
+        bottomField.text = "BOTTOM"
+        bottomField.textAlignment = NSTextAlignment.center
     }
     
+    // MARK: ViewWillAppear
     override func viewWillAppear(_ animated: Bool) {
          takePhotoButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        subscribeToKeyboardNotifications()
     }
+    
+    // MARK: ViewWillDisappear
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    // MARK: Keybaord Functions
+    @objc func keyboardWillShow(_ notification:Notification) {
+        imagePickerView.frame.origin.y = -getKeyboardHeight(notification)
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        imagePickerView.frame.origin.y = 0
+    }
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue //of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)),
+                                               name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)),
+                                               name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    
     
     // MARK: ImagePicker Functions
     //what happens when picking an image and dismissing
@@ -50,6 +98,11 @@ UINavigationControllerDelegate {
         imagePickerView.contentMode = .scaleAspectFill
         dismiss(animated: true, completion: nil)
         }
+    }
+    
+    // MARK: ImagePicker cancelling after picking image
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
     
     // MARK: ImagePicker IBActions
@@ -67,10 +120,7 @@ UINavigationControllerDelegate {
         pickerController.cameraCaptureMode = .photo
         present(pickerController, animated: true, completion: nil)
     }
-
-    //MARK: Text Field Functions
-    NSLog()
-    }
+    
 }
 
     
